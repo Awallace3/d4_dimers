afb08002bec6c39857705ad82c2ce07af739597e
Austin Wallace <austinwallace196@gmail.com>
2024-03-21 14:50:33 -0400

    saptdftd4 components correct

--- plot_saptdft.py
+++ plot_saptdft.py
@@ -9,7 +9,11 @@ def main():
     if 'Geometry_bohr' not in df.columns:
         df = src.misc.make_geometry_bohr_column_df(df)
         df.to_pickle(df_name)
-    print(df.columns.values)
+    # if 'C6' not in df.columns:
+    #     df = src.setup.generate_D4_data(df)
+    #     df.to_pickle(df_name)
+    # print(df.columns.values)
+    print(len(df))
     src.plotting.plotting_setup_dft_ddft(
         df,
         df_name,
